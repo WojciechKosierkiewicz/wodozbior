b2482c2de2c17a97ec648ed5cad3c1a26d57ff73
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2025-05-29 20:33:12 +0200

    zmiany w plikach

--- src/main/java/com/example/wodozbior/repository/DBFacade.java
+++ src/main/java/com/example/wodozbior/repository/DBFacade.java
@@ -1,15 +1,30 @@
 package com.example.wodozbior.repository;
 
+
+import com.example.wodozbior.dto.hydrodata.HydroStationDto;
+import com.example.wodozbior.dto.hydrodata.StationDetailsDto;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
 public class DBFacade {
     static DBFacade instance;
-    private DBFacade(){
-        //TODO
-    }
 
-    public static DBFacade getInstance(){
-        if(instance == null){
-            instance = new DBFacade();
-        }
-        return instance;
-    }
+
+
+     public ArrayList<HydroStationDto> getStations(){
+        //TODO
+         //ZWRACA LISTE WSZYSTKICH STACJI TAK JAK W API
+         //AKTUALNY STAN
+         //WYPEŁNIJ INFO Z DTO
+
+        ArrayList<HydroStationDto> stations = new ArrayList<>();
+        return stations;
+     }
+
+     public StationDetailsDto getStationStationById(int id){
+         StationDetailsDto station = new StationDetailsDto();
+         return station;
+     }
 }
