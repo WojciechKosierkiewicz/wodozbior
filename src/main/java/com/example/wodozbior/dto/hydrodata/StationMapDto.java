c12c044d27e77afd56bdabeaa7bac6fab7d2d543
Wojciech Kosierkiewicz <wojtekkosierkiewicz@gmail.com>
2025-06-04 15:20:58 +0200

    sztuczne dane do działania bez db

--- src/main/java/com/example/wodozbior/dto/hydrodata/StationMapDto.java
+++ src/main/java/com/example/wodozbior/dto/hydrodata/StationMapDto.java
@@ -12,4 +12,14 @@ public class StationMapDto { // dane do wyswietlenia na mapie
     private int waterLevel;
     private String measurementDate;
     private String color; // RGB color code
+
+    public StationMapDto(String measurementDate, String id, String river, double latitude, double longitude, int waterLevel, String color) {
+        this.measurementDate = measurementDate;
+        this.id = id;
+        this.river = river;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.waterLevel = waterLevel;
+        this.color = color;
+    }
 }
